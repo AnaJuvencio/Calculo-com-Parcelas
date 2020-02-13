8ccc24a5e666ff2d1a2e8ebe491838ddf42c5533
anabias <60902514+anabias@users.noreply.github.com>
2020-02-13 20:01:23 -0300

    Initial commit

---
+++ Calcular.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Aula01;
+
+namespace Aula01
+{
+    public class Calcular
+    {
+        public double qtdParcela, valorInicial, valorqFinal, resultado;
+
+        public double CalcularParcelas() 
+        {
+            if(qtdParcela >= 5)
+            {
+                valorqFinal = (valorInicial / qtdParcela);
+                resultado = valorqFinal + (1 * valorqFinal / 100);
+                
+            }
+            return (resultado);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Aula01;
+
+namespace Aula01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calcular calcular = new Calcular();
+
+            Console.Write("Informe o valor inicial da parcela => ");
+            calcular.valorInicial = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe a quantidade de parcelas => ");
+            calcular.qtdParcela = double.Parse(Console.ReadLine());
+
+            Console.Write("O valor final da parcela é => " + calcular.CalcularParcelas());
+
+            Console.ReadKey();
+        }
+    }
+}
